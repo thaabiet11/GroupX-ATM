8982e19b06b673fa6895898bea0e3192ed48c4a0
Thaabiet Abrahams <64799734+thaabiet11@users.noreply.github.com>
2023-06-08 06:07:35 -0700

    Update ATMController.cs
    Updated C# code for ATM project

--- Controllers/ATMController.cs
+++ Controllers/ATMController.cs
@@ -1,41 +1,96 @@
-﻿using ATM_project.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using ATM_API;
+using Group_X_ATM.Models;
+using System.Net.NetworkInformation;
 
-namespace ATM_project.Controllers
+namespace ATM_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [EnableCors]
     public class ATMController : ControllerBase
     {
-        // GET: api/<ATMController>
-        [HttpGet]
-        public IActionResult Get()
+        [HttpGet("balance")]
+        public IActionResult GetBalance()
         {
-            return Ok(new string[] { "value1", "value2" });
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string query = "SELECT Balance FROM transactions ";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        decimal balance = (decimal)command.ExecuteScalar();
+                        return Ok(balance);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-        // GET api/<ATMController>/5
-        [HttpGet("{id}")]
-        public IActionResult Get(int id)
+
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
         {
-            return Ok("value");
+            return new string[] { "value1", "value2" };
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<IEnumerable<SignUp>> Get(int id)
+        {
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string query = "SELECT * FROM login WHERE Pin = @id";
+            List<SignUp> adduser = new List<SignUp>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                SignUp user = new SignUp();
+                                user.Pin = reader.GetInt32(0);
+                                adduser.Add(user);
+                            }
+                        }
+                    }
+                }
+                return Ok(adduser);
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
 
         [HttpPost("signup")]
-        public IActionResult SignUppost([FromBody] SignUp signup)
+        public IActionResult Post([FromBody] SignUp signup)
         {
             if (signup == null)
             {
                 return BadRequest();
             }
-
             string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
             string query = "INSERT INTO signup (FirstName, LastName, Age, ID, Pin) VALUES (@FirstName, @LastName, @Age, @ID, @Pin)";
-
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -46,29 +101,56 @@ namespace ATM_project.Controllers
                         command.Parameters.AddWithValue("@FirstName", signup.FirstName);
                         command.Parameters.AddWithValue("@LastName", signup.LastName);
                         command.Parameters.AddWithValue("@Age", signup.Age);
-                        command.Parameters.AddWithValue("@ID", signup.Id);
+                        command.Parameters.AddWithValue("@ID", signup.ID);
                         command.Parameters.AddWithValue("@Pin", signup.Pin);
                         command.ExecuteNonQuery();
+                        return Ok();
                     }
                 }
-                return Ok();
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
-        [HttpPost("withdrawal")]
-        public IActionResult Withdrawalpost([FromBody] Withdrawal withdrawal)
+
+
+        [HttpPut("{id}")]
+        public IActionResult ChangePinPut(int id, [FromBody] SignUp signUp)
         {
-            if (withdrawal == null)
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string updateSql = "UPDATE signup SET Pin = @NewPin WHERE Pin = @Pin";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(updateSql, connection))
+                    {
+                        command.Parameters.AddWithValue("@NewPin", signUp.Pin);
+                        command.Parameters.AddWithValue("@Pin", id);
+                        int rowsAffected = command.ExecuteNonQuery();
+                        return Ok($"{rowsAffected} rows updated.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("transactions/withdrawal")]
+        public IActionResult WithdrawalPost([FromBody] Transactions transactions)
+        {
+            if (transactions == null)
             {
                 return BadRequest();
             }
-
             string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
-            string query = "INSERT INTO withdrawal (WithdrawalAmount) VALUES (@withdrawalAmount)";
-
+            string query = "INSERT INTO transactions (Deposit, Withdrawal, Balance) VALUES (@Deposit, @Withdrawal, @Balance)";
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -76,32 +158,110 @@ namespace ATM_project.Controllers
                     connection.Open();
                     using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        command.Parameters.AddWithValue("@WithdrawalAmount", withdrawal.withdrawalAmount);
+                        command.Parameters.AddWithValue("@Deposit", transactions.Deposit);
+                        command.Parameters.AddWithValue("@Withdrawal", transactions.Withdrawal);
+                        command.Parameters.AddWithValue("@Balance", transactions.Balance);
                         command.ExecuteNonQuery();
+                        return Ok();
                     }
                 }
-                return Ok();
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
         }
-        
-        [HttpPost]
-        public IActionResult UpdatePin([FromBody] ChangePinRequest request)
+
+
+        [HttpPost("transactions")]
+        public IActionResult DepositPost([FromBody] Transactions transactions)
         {
-            string currentPin = request.CurrentPin;
-            string newPin = request.NewPin;
-            string confirmPin = request.ConfirmPin;
-
-            if (newPin != confirmPin)
+            if (transactions == null)
             {
-                return BadRequest(new { error = "New PIN and Confirm PIN do not match" });
+                return BadRequest();
             }
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string query = "INSERT INTO transactions (Deposit, Withdrawal, Balance) VALUES (@Deposit, @Withdrawal, @Balance)";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Deposit", transactions.Deposit);
+                        command.Parameters.AddWithValue("@Withdrawal", transactions.Withdrawal);
+                        command.Parameters.AddWithValue("@Balance", transactions.Balance);
+                        command.ExecuteNonQuery();
+                        return Ok();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
 
-            string connectionString = @"Data Source=DESKTOP-GMP1P5K;Initial Catalog=atmproject;Integrated Security=True; Encrypt=False";
-            string updateQuery = "UPDATE signup SET Pin = @NewPin WHERE Pin = @CurrentPin";
+
+
+        [HttpPost("transactions/deposit")]
+        public IActionResult MakeDeposit([FromBody] Transactions transactions)
+        {
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string updateBalanceSql = "UPDATE transactions SET Balance = Balance + @Deposit";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(updateBalanceSql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Deposit", transactions.Deposit);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return Ok("Deposit processed successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost("transactions/withdrawal")]
+        public IActionResult MakeWithdrawal([FromBody] Transactions transactions)
+        {
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string updateBalanceSql = "UPDATE transactions SET Balance = Balance - @Withdrawal";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(updateBalanceSql, connection))
+                    {
+                        command.Parameters.AddWithValue("@Withdrawal", transactions.Withdrawal);
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                return Ok("Withdrawal processed successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("transactions/withdrawal")]
+        public IActionResult GetWithdrawal()
+        {
+            string connectionString = @"Data Source=DESKTOP-PERSV88;Initial Catalog=atmproject;Integrated Security=True";
+            string query = "SELECT Withdrawal FROM transactions ORDER BY TransactionId DESC LIMIT 1";
 
             try
             {
@@ -109,26 +269,18 @@ namespace ATM_project.Controllers
                 {
                     connection.Open();
 
-                    using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        command.Parameters.AddWithValue("@NewPin", newPin);
-                        command.Parameters.AddWithValue("@CurrentPin", currentPin);
-
-                        int rowsAffected = command.ExecuteNonQuery();
-
-                        if (rowsAffected == 0)
-                        {
-                            return NotFound(new { error = "Invalid current PIN" });
-                        }
-
-                        return Ok(new { message = "PIN changed successfully" });
+                        double withdrawalAmount = (double)command.ExecuteScalar();
+                        return Ok(withdrawalAmount);
                     }
                 }
             }
             catch (Exception ex)
             {
-                return StatusCode(500, new { error = ex.Message });
+                return BadRequest(ex.Message);
             }
         }
+
     }
 }
